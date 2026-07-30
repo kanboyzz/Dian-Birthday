58cce8d337d120ab307b760d5505bcda4475c582
kanboyzz <kanboyzz69@gmail.com>
2026-07-30 23:15:03 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,67 @@
+const CONFIG = {
+  namaPasangan: "Dian Agustiana",
+
+  namaPengirim: "Kannn",
+
+  tanggalBuka: "2026-08-07T00:00:00",
+
+  pesanKunci: "Aku ada kejutan buat kamu, tapi kamu harus sabar nunggu tanggalnya ya",
+
+  taglineHero:
+    "Hai sayang, aku mungkin belum bisa kasih hadiah fisik buat kamu dalam waktu dekat. Tapi aku harap kamu suka sama apa yang aku buat untuk sekarang ini. I ALWAYS LOVE YOU SO MUCH MORE THAN ANYTHING!!",
+};
+
+const FOTO_HERO = [
+  { src: "assets/foto 103.jpg", caption: "First date kita" },
+  { src: "assets/foto 7.jpg", caption: "Liat satu foto kamu aja aku salting, ini ada empat" },
+  { src: "assets/foto 15.jpg", caption: "Manis banget sayaaang" },
+  { src: "assets/foto 18.jpg", caption: "Bahkan filter blur aja gabisa nandingin kecantikan kamu" },
+];
+
+const FOTO_GALERI = [
+  { src: "assets/foto 1.jpg" },
+  { src: "assets/foto 31.jpg" },
+  { src: "assets/foto 100.jpg" },
+  { src: "assets/foto 3.jpg" },
+  { src: "assets/foto 15.jpg" },
+  { src: "assets/foto 18.jpg" },
+  { src: "assets/foto 101.jpg" },
+  { src: "assets/foto 32.jpg" },
+  { src: "assets/foto 34.jpg" },
+  { src: "assets/foto 105.jpg" },
+  { src: "assets/foto 33.jpg" },
+  { src: "assets/foto 37.jpg" },
+  { src: "assets/foto 38.jpg" },
+  { src: "assets/foto 104.jpg" },
+  { src: "assets/foto 5.jpg" },
+];
+
+const DAFTAR_ALASAN = [
+  { emoji: "😄", teks: "Karena kamu lebih KECIL dari akuuu" },
+  { emoji: "🍜", teks: "Kamu suka mam seblak, sedangkan aku suka kamu. Gimana dong?" },
+  { emoji: "🎬", teks: "Kamu suka toy story, but we build love story" },
+  { emoji: "😂", teks: "Kamu lucu kalau lagi ngambek gajelas, tapi gasuka kalau kamu ngambek lama-lama" },
+  { emoji: "🤗", teks: "Aku nyaman setiap deket kamu" },
+  { emoji: "🌧️", teks: "Kamu selalu dukung aku walau aku lagi sulit, bahkan kamu selalu sabar hadapin sikap jelek aku sama kamu" },
+  { emoji: "🥳", teks: "Kamu itu lucu, gemesin, imut juga, dan kamu itu cewe tercantik yang aku temui" },
+  { emoji: "💖", teks: "I ALWAYS LOVE YOU SO MUCH MORE THAN ANYTHING SAYAAAAANG!!!" },
+];
+
+const FOTO_ALASAN = [
+  { src: "assets/foto 12.jpg" },
+  { src: "assets/foto 16.jpg" },
+];
+
+const FOTO_SURAT = [
+  { src: "assets/foto 25.jpg" },
+  { src: "assets/foto 22.jpg" },
+];
+
+const FOTO_PENUTUP = [
+  { src: "assets/foto 29.jpg" },
+  { src: "assets/foto 30.jpg" },
+  { src: "assets/foto 35.jpg" },
+  { src: "assets/foto 37.jpg" },
+  { src: "assets/foto 6.jpg" },
+  { src: "assets/foto 10.jpg" },
+];
